f97071572a8b3f0d73c1d8fa76ba48c0be9dcab6
Wil Pirino <wpirino1@gmail.com>
2019-04-23 18:49:08 -0600

    changed MONGODB_URI

--- src/services/database/database.service.ts
+++ src/services/database/database.service.ts
@@ -3,12 +3,11 @@ import { Db, MongoClient } from 'mongodb';
 
 const DB_NAME = 'restaurant-payments';
 const DB_BASE_URL =  process.env.MONGODB_URI || 'mongodb://localhost:27017';
-const DB_FULL_URL =  `${DB_BASE_URL}/${DB_NAME}`;
 
 @Injectable()
 export class DatabaseService {
   private _db: Promise<Db> = new Promise((resolve, reject) => {
-    MongoClient.connect(DB_FULL_URL, { useNewUrlParser: true }, (err, client) => {
+    MongoClient.connect(DB_BASE_URL, { useNewUrlParser: true }, (err, client) => {
       if (err) { reject(err); }
       else { resolve(client.db(DB_NAME)); }
     });
