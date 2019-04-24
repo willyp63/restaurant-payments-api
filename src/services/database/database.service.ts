7bb42f1e8519d8dcbd2bbfdc149346822d818953
Wil Pirino <wpirino1@gmail.com>
2019-04-23 18:03:36 -0600

    updates for heroku

--- src/services/database/database.service.ts
+++ src/services/database/database.service.ts
@@ -2,12 +2,13 @@ import { Injectable } from '@nestjs/common';
 import { Db, MongoClient } from 'mongodb';
 
 const DB_NAME = 'restaurant-payments';
-const DB_URL = `mongodb://localhost:27017/${DB_NAME}`;
+const DB_BASE_URL =  process.env.MONGODB_URI || 'mongodb://localhost:27017';
+const DB_FULL_URL =  `${DB_BASE_URL}/${DB_NAME}`;
 
 @Injectable()
 export class DatabaseService {
   private _db: Promise<Db> = new Promise((resolve, reject) => {
-    MongoClient.connect(DB_URL, { useNewUrlParser: true }, (err, client) => {
+    MongoClient.connect(DB_FULL_URL, { useNewUrlParser: true }, (err, client) => {
       if (err) { reject(err); }
       else { resolve(client.db(DB_NAME)); }
     });
