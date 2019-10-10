94be612e0398baee5d141613d745b5211872678e
Pirino, Wil <wil.pirino@transamerica.com>
2019-10-10 11:43:03 -0600

    disable websockets

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,8 @@ import { TableController } from './controllers/table/table.controller';
 import { TableItemController } from './controllers/table-item/table-item.controller';
 import { UserController } from './controllers/user/user.controller';
 
-import { UserGateway } from './gateways/user/user.gateway';
-import { TableItemGateway } from './gateways/table-item/table-item.gateway';
+// import { UserGateway } from './gateways/user/user.gateway';
+// import { TableItemGateway } from './gateways/table-item/table-item.gateway';
 
 import { DatabaseService } from './services/database/database.service';
 import { TableService } from './services/table/table.service';
